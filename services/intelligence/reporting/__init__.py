c04daf36ec39acae8496442b9d2fad991fee4c3c
Uwakwe Chukwuerika Paul <uwakwechukwuebukapaul@gmail.com>
2026-08-08 22:51:22 +0100

    Export intelligence reporting components

--- services/intelligence/reporting/__init__.py
+++ services/intelligence/reporting/__init__.py
@@ -1,3 +1,24 @@
 """
-Sentinel DNA Investigation Reporting Module
+Sentinel DNA Intelligence Reporting Package
+
+Exports investigation reporting components.
 """
+
+from services.intelligence.reporting.executive_summary import (
+    ExecutiveSummaryGenerator,
+)
+
+from services.intelligence.reporting.timeline_builder import (
+    TimelineBuilder,
+)
+
+from services.intelligence.reporting.report_generator import (
+    ReportGenerator,
+)
+
+
+__all__ = [
+    "ExecutiveSummaryGenerator",
+    "TimelineBuilder",
+    "ReportGenerator",
+]
